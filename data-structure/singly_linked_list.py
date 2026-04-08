8e09171a57231756d69f173598e795dfe5cc2b28
rlatm <rlatmdgus388@naver.com>
2026-04-08 14:32:00 +0900

    add deleteNode to singly_linked_list

--- data-structure/singly_linked_list.py
+++ data-structure/singly_linked_list.py
@@ -20,27 +20,45 @@ def insertNode(findData, insertData):
     if head.data == findData:   # 첫 번째 노드에 삽입인 경우
         node = Node()
         node.data = insertData
-        node.link = head
-        head = node
+        node.link = head    # 새 노드의 link를 다음 노드로 연결
+        head = node         # head를 새 노드로 변경
         return
         
     # 중간 노드에 삽입하는 경우
+    current = head      # 위 if문을 통과 -> 첫 번째 노드에 삽입이 아닌 경우
+    while current.link != None:
+        pre = current
+        current = current.link          # currnet를 다음 노드로 한 칸 이동
+        if current.data == findData:    # current가 삽입하려는 데이터의 위치이면
+            node = Node()
+            node.data = insertData
+            node.link = current         # 새 노드의 link를 current에 연결
+            pre.link = node             # 새 노드 이전 link를 새 노드에 연결
+            return
+        
+    # 마지막 노드 삽입
+    node = Node()                       # 반복문을 전부 돌때까지 findData를 못찾을 시, 맨 뒤에 새 노드 생성.
+    node.data = insertData
+    current.link = node
+
+def deleteNode(deleteData):
+    global memory, head, current, pre
+    
+    if head.data == deleteData:
+        current = head
+        head = head.link
+        del(current)
+        return
+    
     current = head
     while current.link != None:
         pre = current
         current = current.link
-        if current.data == findData:
-            node = Node()
-            node.data = insertData
-            node.link = current
-            pre.link = node
+        if current.data == deleteData:
+            pre.link = current.link
+            del(current)
             return
         
-    # 마지막 노드 삽입
-    node = Node()
-    node.data = insertData
-    current.link = node
-            
 
 
 memory = []     # 전역 변수 선언
