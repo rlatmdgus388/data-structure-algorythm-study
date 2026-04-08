235c262eed01b12d914fff6159257f6922c15f58
rlatm <rlatmdgus388@naver.com>
2026-04-08 13:43:27 +0900

    add inserttNode func to singly_linked_list

--- Data_Structure/singly_linked_list.py
+++ Data_Structure/singly_linked_list.py
@@ -10,11 +10,40 @@ def printNodes(start):          # 노드 출력 함수
         return
     print(current.data, end=' ')
     while current.link != None:
-        crrent = current.link
+        current = current.link
         print(current.data, end = ' ')
     print()
+
+def insertNode(findData, insertData):
+    global memory, head, current, pre
     
-memory = []
+    if head.data == findData:   # 첫 번째 노드에 삽입인 경우
+        node = Node()
+        node.data = insertData
+        node.link = head
+        head = node
+        return
+        
+    # 중간 노드에 삽입하는 경우
+    current = head
+    while current.link == None:
+        pre = current
+        current = current.link
+        if current.data == findData:
+            node = Node()
+            node.data = insertData
+            node.link = current
+            pre.link = node
+            return
+        
+    # 마지막 노드 삽입
+    node = Node()
+    node.data = insertData
+    current.link = node
+            
+
+
+memory = []     # 전역 변수 선언
 head, current, pre = None, None, None
 dataArray = [1, 2, 3, 4, 5]
 
