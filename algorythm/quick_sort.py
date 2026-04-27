79e1005d1f0b82c7c12334af8e39cf3c0894beae
rlatm <rlatmdgus388@naver.com>
2026-04-27 22:16:29 +0900

    d

--- algorythm/quick_sort.py
+++ algorythm/quick_sort.py
@@ -44,7 +44,7 @@ def quick_sort(array, start, end):
         while (left <= end and array[left] <= array[pivot]):
             left += 1
         while (right > start and array[right] >= array[pivot]):
-            right -= 1:
+            right -= 1
         if (left > right):
             array[right], array[pivot] = array[pivot], array[right]
         else:
